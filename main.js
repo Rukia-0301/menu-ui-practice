5b362b9968be79de74da7d4a8e11e75e4443b9fa
Rukia-0301 <chii-0301r@docomo.ne.jp>
2026-08-04 10:36:37 +0900

    navからもタブメニューが開けるよう変更

--- main.js
+++ main.js
@@ -1,21 +1,48 @@
 'use strict';
 
 {
-  const menuItem = document.querySelectorAll('.menu ul li a');
+  const menuItems = document.querySelectorAll('.menu ul li a');
   const contents = document.querySelectorAll('.content');
+  const navItems = document.querySelectorAll('.nav-items .nav-item');
 
-  menuItem.forEach((item) => {
+  const switchTab = (id) => {
+    menuItems.forEach((item) => {
+      item.classList.remove('active');
+    });
+    contents.forEach((content) => {
+      content.classList.remove('active');
+    });
+
+    document.querySelector(`.menu ul li a[data-id="${id}"]`).classList.add('active');
+    document.querySelector(`#${id}`).classList.add('active');
+  }
+
+  menuItems.forEach((item) => {
     item.addEventListener('click', (e) => {
       e.preventDefault();
-      menuItem.forEach((item) => {
-        item.classList.remove('active');
-      });
+      switchTab(item.dataset.id);
       item.classList.add('active');
-
-      contents.forEach((content) => {
-        content.classList.remove('active');
-      });
-      document.querySelector('#' + item.dataset.id).classList.add('active');
     });
   });
+
+  navItems.forEach((item) => {
+    item.addEventListener('click', (e) => {
+      switchTab(item.dataset.id);
+    });
+  });
+
+  // menuItem.forEach((item) => {
+  //   item.addEventListener('click', (e) => {
+  //     e.preventDefault();
+  //     menuItem.forEach((item) => {
+  //       item.classList.remove('active');
+  //     });
+  //     item.classList.add('active');
+
+  //     contents.forEach((content) => {
+  //       content.classList.remove('active');
+  //     });
+  //     document.querySelector('#' + item.dataset.id).classList.add('active');
+  //   });
+  // });
 }
